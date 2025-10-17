9b76f8103ba2ed4831fefec8e1fe4b91fd9e89c4
Gerald_Putra <geraldsquerpens11@gmail.com>
2025-10-17 14:39:02 +0700

    menambahkan folder dashboard
    menambahkan folder v_dashboard_siswa

---
+++ app/Views/dashboard_siswa/v_dashboard_siswa.php
@@ -0,0 +1,63 @@
+<div class="row">
+  <!-- Box 1: Jumlah Tugas -->
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="info-box">
+      <span class="info-box-icon bg-info elevation-1">
+        <i class="fa-solid fa-book-open"></i>
+      </span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Tugas Aktif</span>
+        <span class="info-box-number">
+          5
+          <small>tugas</small>
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Box 2: Nilai Rata-rata -->
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="info-box mb-3">
+      <span class="info-box-icon bg-success elevation-1">
+        <i class="fa-solid fa-chart-line"></i>
+      </span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Nilai Rata-rata</span>
+        <span class="info-box-number">87.5</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- fix for small devices only -->
+  <div class="clearfix hidden-md-up"></div>
+
+  <!-- Box 3: Kehadiran -->
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="info-box mb-3">
+      <span class="info-box-icon bg-warning elevation-1">
+        <i class="fa-solid fa-user-check"></i>
+      </span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Kehadiran</span>
+        <span class="info-box-number">95%</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Box 4: Pengumuman -->
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="info-box mb-3">
+      <span class="info-box-icon bg-danger elevation-1">
+        <i class="fa-solid fa-bullhorn"></i>
+      </span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Pengumuman Baru</span>
+        <span class="info-box-number">3</span>
+      </div>
+    </div>
+  </div>
+</div>
