113b5eb1a653cf261d2b48c74ab97c2b23372c93
Gerald_Putra <geraldsquerpens11@gmail.com>
2025-10-19 21:07:17 +0700

    Update v_index.php

--- app/Views/dashboard_siswa/tugas/v_index.php
+++ app/Views/dashboard_siswa/tugas/v_index.php
@@ -1,1 +1,34 @@
-<h1>T U G A S</h1>
+<div class="card">
+  <div class="card-header bg-info text-white">
+    <h3 class="card-title">Daftar Tugas</h3>
+  </div>
+  <div class="card-body">
+    <table class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Judul Tugas</th>
+          <th>Mata Pelajaran</th>
+          <th>Tenggat</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Tugas Algoritma</td>
+          <td>Informatika</td>
+          <td>20 Oktober 2025</td>
+          <td><span class="badge badge-success">Selesai</span></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Rangkuman Jaringan</td>
+          <td>TIK</td>
+          <td>22 Oktober 2025</td>
+          <td><span class="badge badge-warning">Belum</span></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
