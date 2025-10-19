69354c7d62fe1655565e69362c5f8a03affaa895
Gerald_Putra <geraldsquerpens11@gmail.com>
2025-10-19 20:52:05 +0700

    Ubah index dashboard siswa
    Mengbah index dashboard siswa

--- app/Views/dashboard_siswa/materi/v_index.php
+++ app/Views/dashboard_siswa/materi/v_index.php
@@ -1,1 +1,43 @@
-<h1>M A T E R I</h1>
+<div class="card">
+  <div class="card-header bg-warning text-white">
+    <h3 class="card-title">Materi Pelajaran</h3>
+  </div>
+
+  <div class="card-body">
+    <table class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Judul Materi</th>
+          <th>Mata Pelajaran</th>
+          <th>Guru Pengajar</th>
+          <th>File / Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Pengenalan Jaringan Komputer</td>
+          <td>TIK</td>
+          <td>Pak Dwi Santoso</td>
+          <td>
+            <a href="#" class="btn btn-sm btn-primary">
+              <i class="fa-solid fa-download"></i> Unduh
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Algoritma Dasar</td>
+          <td>Informatika</td>
+          <td>Bu Rani</td>
+          <td>
+            <a href="#" class="btn btn-sm btn-primary">
+              <i class="fa-solid fa-link"></i> Lihat
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
